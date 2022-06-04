c6970820544b7408c02a348afb1875f27e0ae81a
Youen Plusquellec <youenetleo@gmail.com>
2022-06-04 15:27:02 +0200

    Add ultimate text to image

--- kanji/kanjiMessage.js
+++ kanji/kanjiMessage.js
@@ -2,8 +2,10 @@ const KanjiRepository = require('../model/kanjiRepository');
 const kanjiRepository = new KanjiRepository();
 
 const textToImage = require('text-to-image');
+const { UltimateTextToImage } = require('ultimate-text-to-image');
 const logger = require('../common/utils/logger');
 const config = require('../config');
+const path = require("path");
 
 const { MessageEmbed, MessageAttachment } = require('discord.js');
 const { stripIndents } = require('common-tags');
@@ -27,7 +29,7 @@ module.exports = {
 		logger.debug("Random Kanji found : " + randKanji.kanji);
 
 		// It's generating an image from the kanji.
-		const dataUri = await textToImage.generate(randKanji.kanji, {
+		/*const dataUri = await textToImage.generate(randKanji.kanji, {
 			fontSize: config.font_size,
 			margin: 5,
 			bgColor: '#F4E0C7',
@@ -40,7 +42,23 @@ module.exports = {
 		var base64Data = dataUri.replace(/^data:image\/png;base64,/, "");
 		require("fs").writeFile("out.png", base64Data, 'base64', function (err) {
 			if (err) logger.error(`write file error: ${err}`);
-		});
+		});*/
+
+		const textToImage = new UltimateTextToImage(randKanji.kanji, {
+			//width: 400,
+			fontFamily: "Arial, Sans",
+			fontColor: "#000000",
+			fontSize: 390,
+			minFontSize: 10,
+			lineHeight: 50,
+			autoWrapLineHeightMultiplier: 1.2,
+			margin: 20,
+			align: "center",
+			valign: "middle",
+			backgroundColor: "F4E0C7",
+		})
+		.render()
+		.toFile(path.join(__dirname, "../out.png"));
 
 		// It's creating an embed with the information about the kanji.
 		const kanjiEmbed = new MessageEmbed()
