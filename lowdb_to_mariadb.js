a67aaf588e6825dafb8e6df7ccf6702528e36e72
Youen Plusquellec <youenetleo@gmail.com>
2022-07-02 19:00:01 +0200

    async to sync

--- lowdb_to_mariadb.js
+++ lowdb_to_mariadb.js
@@ -9,19 +9,19 @@ const kanjiRepository = new KanjiRepository();
 const ActionRepository = require('./deprecated/model/actionRepository');
 const actionRepository = new ActionRepository();
 
+async function manageData() {
 
-const kanjis = kanjiRepository.getKanjis()
-kanjis.forEach(kanji => {
-    const res = kanjiModel.addKanji(kanji.kanji, kanji.available)
-    res.then(result => {
-        console.log(result)
-    })
-})
+    const kanjis = kanjiRepository.getKanjis()
+    for (let i=0; i<kanjis.length; i++) {
+        const res = await kanjiModel.addKanji(kanjis[i].kanji, kanjis[i].available)
+        console.log(res)
+    }
+    
+    const actions = actionRepository.getActions()
+    for (let i=0; i<actions.length; i++) {
+        const res = actionModel.createAction('792912664262606848', '191879625972908032', actions[i].type, actions[i].cron, actions[i].channel_id, actions[i].mention_role)
+        console.log(res)
+    }
+}
 
-const actions = actionRepository.getActions()
-actions.forEach(action => {
-    const res = actionModel.createAction('792912664262606848', '191879625972908032', action.type, action.cron, action.channel_id, action.mention_role)
-    res.then(result => {
-        console.log(result)
-    })
-})
+manageData();
