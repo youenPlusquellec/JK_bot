d837b6205b24041c27068bfe17c54fe9414edb65
Youen Plusquellec <youenetleo@gmail.com>
2022-08-15 20:21:14 +0200

    :card_file_box: Add sql fix for scehduled message

--- database/v1.3_update.sql
+++ database/v1.3_update.sql
@@ -15,6 +15,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+--
+-- Update Table structure for table `action`
+--
+ALTER TABLE `action` ADD parameters json NULL;
+
 --
 -- Table structure for table `vocabulary`
 --
