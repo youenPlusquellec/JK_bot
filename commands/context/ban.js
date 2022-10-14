dbd2af73e394f4cf76bf7d006ef2670acb3e95c2
Youen Plusquellec <youenetleo@gmail.com>
2022-10-14 23:52:10 +0200

    :sparkles: Change ban duration

--- commands/context/ban.js
+++ commands/context/ban.js
@@ -40,6 +40,6 @@ module.exports = class Ban extends Command {
 		logger.info(`User '${user.username}' banned with message : ${message}`);
 
 		await interaction.reply(message);
-		interaction.options.getMember('user').ban({ days: 7, reason: 'Ban rigolo' });
+		interaction.options.getMember('user').ban({ reason: "Parce qu'il le mérite" });
 	}
 };
